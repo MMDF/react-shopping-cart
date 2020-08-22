32b42fafe6fa1ef7271afd3c481d4b44c4307409
Mehmet Efe Akça <efeakca@gmail.com>
2020-08-22 22:53:10 +0300

    Minor adjustments

--- src/components/Shelf/ProductList/Product/index.js
+++ src/components/Shelf/ProductList/Product/index.js
@@ -20,7 +20,7 @@ const Product = ({ product, addProduct }) => {
       <Thumb
         classes="shelf-item__thumb"
         // src={require(`../../../../static/products/${product.sku}_1.jpg`)}
-        alt={product.title}
+        // alt={product.title}
       />
       <p className="shelf-item__title">{product.title}</p>
       <div className="shelf-item__price">
